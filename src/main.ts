eef689a8937176247ff2120ee2318b0cf3741f06
daisuke motohashi <ddssk.m@gmail.com>
2023-01-18 14:11:09 +0900

    ステップ描画を実装

--- src/main.ts
+++ src/main.ts
@@ -139,20 +139,31 @@ class MazeDrawer {
     border_outline: number
     // x_max: number
     // y_max: number
+    finished: boolean
+
+    pointer: number
 
     constructor(b: MazeBuilder, panel_size: number, outline: number = 10) {
         this.builder = b
         this.panel_size = panel_size
         this.border_outline = outline
         this.buffer = outline + panel_size / 2
+        this.finished = false
+        this.pointer = 0
     }
 
-    draw(p: p5) {
+    init(p: p5) {
         p.rectMode(p.CENTER)
         p.fill(0, 255, 0)
         this.draw_box(p)
         p.fill(0, 0, 0)
-        this.draw_maze(p)
+    }
+
+    draw(p: p5) {
+        if (!this.finished) {
+            // this.draw_maze(p)    // 一括病が
+            this.draw_step(p)       // １マスずつ描画
+        }
     }
 
     draw_box(p: p5) {
@@ -169,12 +180,28 @@ class MazeDrawer {
         // p.rect(0, 0, width * this.panel_size, height * this.panel_size)
     }
 
+    /**
+     * 迷路を一括で描画する
+     * @param p p5オブジェクト
+     */
     draw_maze(p: p5) {
         this.builder.floor.forEach((row, y_index) => {
             const line = row.map((f, x_index) => {
                 this.draw_block(p, f, x_index, y_index)
             })
         });
+    }
+
+    /**
+     * 生成済みの迷路を１マスずつ描画していく
+     * @param p p5オブジェクト
+     */
+    draw_step(p: p5) {
+        const point_x = this.pointer % this.builder.width
+        const point_y = Math.floor(this.pointer / this.builder.width)
+        const f = this.builder.floor[point_y][point_x]
+        this.draw_block(p, f, point_x, point_y)
+        this.pointer++
     }
 
     draw_block(p: p5, f: number, x: number, y: number) {
@@ -218,12 +245,10 @@ const sketch = (p: p5) => {
         builder.debug_print_maze()
 
         drawer = new MazeDrawer(builder, 50)
+        drawer.init(p)
     };
 
     p.draw = () => {
-        p.background(220);
-        p.ellipse(50, 50, 80, 80);
-
         drawer.draw(p)
     };
 
