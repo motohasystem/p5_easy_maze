d6d9e0e56b6125d04e8cde3b81e21fc87721ed73
daisuke motohashi <ddssk.m@gmail.com>
2023-02-25 15:56:47 +0900

    連続実行できるようにフォームとボタンを追加

--- src/main.ts
+++ src/main.ts
@@ -4,63 +4,78 @@ import { MazeDrawer } from "./maze_drawer";
 
 import p5 from "p5";
 
-const sketch = (p: p5) => {
-    const BLOCK_SIZE = 20
-    const MAZE_X = 8
-    const MAZE_Y = 8
+const generate_sketch = (input_width: number, input_height: number, input_block: number) => {
+    return (p: p5) => {
+        const BLOCK_SIZE = input_block
+        const MAZE_X = input_width
+        const MAZE_Y = input_height
 
-    // const BLOCK_SIZE = 24
-    // const MAZE_X = 45
-    // const MAZE_Y = 15
+        const BORDER_WIDTH = 20
 
-    // const BLOCK_SIZE = 24
-    // const MAZE_X = 65
-    // const MAZE_Y = 35
+        let width = MAZE_X
+        let height = MAZE_Y
+        if (width % 2 == 0) {
+            width++
+        }
+        if (height % 2 == 0) {
+            height++
+        }
 
-    // const BLOCK_SIZE = 12
-    // const MAZE_X = 65
-    // const MAZE_Y = 65
+        const builder = new MazeBuilder(width, height)
 
-    // const BLOCK_SIZE = 6
-    // const MAZE_X = 159
-    // const MAZE_Y = 159
+        let drawer: MazeDrawer
 
-    const BORDER_WIDTH = 20
+        p.setup = () => {
+            const canvas_width = BORDER_WIDTH + width * BLOCK_SIZE
+            const canvas_height = BORDER_WIDTH + height * BLOCK_SIZE
 
-    let width = MAZE_X
-    let height = MAZE_Y
-    if (width % 2 == 0) {
-        width++
-    }
-    if (height % 2 == 0) {
-        height++
-    }
+            // p.createCanvas(canvas_width, canvas_height);
+            console.log({ canvas_width })
+            console.log({ canvas_height })
+            p.createCanvas(canvas_width, canvas_height);
 
-    const builder = new MazeBuilder(width, height)
+            builder.pile_driving()
+            builder.make_maze()
+            builder.debug_print_maze()
 
-    let drawer: MazeDrawer
+            drawer = new MazeDrawer(builder.get_maze(), BLOCK_SIZE)
+            drawer.init(p)
+        };
 
-    p.setup = () => {
-        const canvas_width = BORDER_WIDTH + width * BLOCK_SIZE
-        const canvas_height = BORDER_WIDTH + height * BLOCK_SIZE
+        p.draw = () => {
+            drawer.draw(p)
+        };
 
-        // p.createCanvas(canvas_width, canvas_height);
-        console.log({ canvas_width })
-        console.log({ canvas_height })
-        p.createCanvas(canvas_width, canvas_height);
-
-        builder.pile_driving()
-        builder.make_maze()
-        builder.debug_print_maze()
-
-        drawer = new MazeDrawer(builder.get_maze(), BLOCK_SIZE)
-        drawer.init(p)
     };
+}
 
-    p.draw = () => {
-        drawer.draw(p)
-    };
+const get_value_as_number = (id: string): number => {
+    const input = document.getElementById(id) as HTMLInputElement
+    return parseInt(input.value)
+}
+const btn_run = document.getElementById("button_run") as HTMLInputElement
+let instance: p5 | undefined = undefined;
 
-};
+btn_run.addEventListener('click', () => {
+    // フォーカスを外す
+    const ae = document.activeElement as HTMLInputElement
+    if (ae) {
+        ae.blur()
+    }
 
-new p5(sketch);
+    const maze_width = get_value_as_number("maze_width")
+    const maze_height = get_value_as_number("maze_height")
+    const boxsize = get_value_as_number("maze_boxsize")
+
+    if (instance == undefined) {
+        const sketch = generate_sketch(maze_width, maze_height, boxsize)
+        instance = new p5(sketch);
+        ae.value = 'stop'
+    }
+    else {
+        instance.remove()
+        instance.clear(1, 1, 1, 0)
+        instance = undefined
+        ae.value = 'run'
+    }
+})
